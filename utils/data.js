cf5097b2e4335930433ccb2c550acf2de4fb1e56
LCatSew <119518206+LCatSew@users.noreply.github.com>
2023-07-25 00:18:12 -0500

    added data to the data js file

---
+++ utils/data.js
@@ -0,0 +1,21 @@
+const usernames = [
+    'lernantino',
+    'amiko',
+    'skaterboi',
+    'dylan2',
+];
+
+const thoughts = [
+    'I love coding!',
+    'I love pizza!',
+    'I love my dog!',
+    'I love my cat!',
+];
+
+const reactions = [
+    '😆',
+    '😍',
+    '😡',
+    '👍',
+    '👎',
+];
